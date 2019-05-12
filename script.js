1c7a953a5a6bccde6df98746d68c2375e84d6bd8
Reno Postema <postemareno@gmail.com>
2019-05-12 11:56:57 +0200

    create first javascript object

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+function Monkey(name, food, age, specie) {
+  this.monkeyName = name;
+  this.monkeyFood= food;
+  this.monkeyAge = age;
+  this.monkeySpecie= specie;
+}
+
+Monkey.prototype.loves = "Bananas";
+
+var monkey01 = new Monkey("Mike", "Bananas", 38, "Gorilla");
+document.getElementById("monkeys").innerHTML =
+"Monkey " + monkey01.monkeyName + ' loves ' + monkey01.loves  + ' his age is ' + monkey01.monkeyAge;
+</script>
