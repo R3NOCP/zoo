25fc1ce05bcab9112a6e09570e5e819dc9f008a6
Reno Postema <postemareno@gmail.com>
2019-05-12 12:26:43 +0200

    Add lions, sex, sound

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-function Monkey(name, food, age, specie) {
+
+// Monkeys
+function Monkey(name, food, age, sex, specie) {
   this.monkeyName = name;
   this.monkeyFood= food;
   this.monkeyAge = age;
+  this.monkeySex = sex;
   this.monkeySpecie= specie;
 }
 
 Monkey.prototype.loves = "Bananas";
+Monkey.prototype.sound = "hoo hoo hooo";
 
-var monkey01 = new Monkey("Mike", "Bananas", 38, "Gorilla");
+var monkey01 = new Monkey("Mike", "Bananas", 38, "male", "Gorilla");
 document.getElementById("monkeys").innerHTML =
-"Monkey " + monkey01.monkeyName + ' loves ' + monkey01.loves  + ' his age is ' + monkey01.monkeyAge;
+monkey01.monkeyName + ' loves ' + monkey01.loves  + ' his age is ' + monkey01.monkeyAge;
+
+
+// Lions
+function Lion(name, food, age, sex, specie) {
+  this.lionName = name;
+  this.lionFood= food;
+  this.lionAge = age;
+  this.lionSex = sex;
+  this.lionSpecie= specie;
+}
+
+Lion.prototype.loves = "fresh meat";
+Lion.prototype.sound = "Roarrrr";
+
+var lion01 = new Lion("Lion01", "Animals", 8, "male", "Lion");
+document.getElementById("lions").innerHTML =
+lion01.lionName + ' loves ' + lion01.loves  + ' his age is ' + lion01.lionAge;
