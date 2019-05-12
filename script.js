931a3bf67375162a3e03be0ae7c22fa836c34ef9
Reno Postema <postemareno@gmail.com>
2019-05-12 12:02:23 +0200

    create html and remove script tags

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script>
 function Monkey(name, food, age, specie) {
   this.monkeyName = name;
   this.monkeyFood= food;
@@ -11,4 +10,3 @@ Monkey.prototype.loves = "Bananas";
 var monkey01 = new Monkey("Mike", "Bananas", 38, "Gorilla");
 document.getElementById("monkeys").innerHTML =
 "Monkey " + monkey01.monkeyName + ' loves ' + monkey01.loves  + ' his age is ' + monkey01.monkeyAge;
-</script>
